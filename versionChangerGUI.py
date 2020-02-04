6dea49dc1033325a63930881f0ce00567bda417d
Wirgiliusz <kamx98@gmail.com>
2020-02-05 00:52:03 +0100

    Code cleaning and commenting

--- versionChangerGUI.py
+++ versionChangerGUI.py
@@ -6,35 +6,40 @@ from PIL import ImageTk, Image
 
 
 class Application:
+    # Getting OS independent game.exe paths
     orginal_exe_path = pathlib.Path("./Celeste_Test/orgi/CelesteEXE")
     everest_exe_path = pathlib.Path("./Celeste_Test/oeve/CelesteEXE")
     destination_path = pathlib.Path("./Celeste_Test/CelesteEXE")
+    # Getting OS independent icon images paths
     orginal_icon_path = pathlib.Path("./Icons/celeste_icon.jpg")
     everest_icon_path = pathlib.Path("./Icons/everest_icon.jpg")
     load_image_orginal = None
     load_image_everest = None
     icon = None
     icon_Label = None
-    actualVersion = "Unknown"
+    actualVersion = "Unknown" # Stores what version is choosen
 
     def changeToOrginal(self):
-        shutil.copy2(self.orginal_exe_path, self.destination_path)
+        shutil.copy2(self.orginal_exe_path, self.destination_path) # Copies orginal game.exe to main gamefolder
         self.actualVersion = "Orginal"
-        self.icon = ImageTk.PhotoImage(self.load_image_orginal)
-        self.icon_Label.config(image=self.icon)
-        print("Game switched to Orginal version")
+        self.updateIcons()
 
     def changeToEverest(self):
-        shutil.copy2(self.everest_exe_path, self.destination_path)
+        shutil.copy2(self.everest_exe_path, self.destination_path) # Copies modded game.exe to main gamefolder
         self.actualVersion = "Everest"
-        self.icon = ImageTk.PhotoImage(self.load_image_everest)
-        self.icon_Label.config(image=self.icon)
-        print("Game switched to Everest version")
+        self.updateIcons()
+
+    def updateIcons(self):
+        if(self.actualVersion == "Orginal"):
+            self.icon = ImageTk.PhotoImage(self.load_image_orginal)
+            self.icon_Label.config(image=self.icon)
+        elif(self.actualVersion == "Everest"):
+            self.icon = ImageTk.PhotoImage(self.load_image_everest)
+            self.icon_Label.config(image=self.icon)
 
     def __init__(self, root):
-        root.geometry("360x90")
+        root.geometry("365x90")
         root.title("Celeste version changer")
-
         self.load_image_orginal = Image.open(self.orginal_icon_path)
         self.load_image_everest = Image.open(self.everest_icon_path)
 
