ffbad43e23a425f1ead218b903019b21eeb61f7c
Wirgiliusz <kamx98@gmail.com>
2020-02-04 21:44:21 +0100

    GUI implementation

---
+++ versionChangerGUI.py
@@ -0,0 +1,34 @@
+import shutil
+import pathlib
+import tkinter as tk
+from tkinter import filedialog as fd
+from tkinter import ttk
+
+
+class Application:
+    orginal_exe_path = pathlib.Path("./Celeste_Test/orgi/CelesteEXE")
+    everest_exe_path = pathlib.Path("./Celeste_Test/oeve/CelesteEXE")
+    destination_path = pathlib.Path("./Celeste_Test/CelesteEXE")
+    actualVersion = "Unknown"
+
+    def changeToOrginal(self):
+        shutil.copy2(self.orginal_exe_path, self.destination_path)
+        print("Game switched to Orginal version")
+
+    def changeToEverest(self):
+        shutil.copy2(self.everest_exe_path, self.destination_path)
+        print("Game switched to Everest version")
+
+    def __init__(self, root):
+        root.geometry("700x300")
+        root.title("Celeste version changer")
+
+        ttk.Label(text="What game version do you want to play?").grid(row=0,column=0,columnspan=2)
+        ttk.Button(command=self.changeToOrginal, text="Orginal Celeste", width=15).grid(row=1,column=1,padx=5,pady=5)
+        ttk.Button(command=self.changeToEverest, text="Everest Celeste", width=15).grid(row=1,column=2,padx=5,pady=5)
+
+
+# Main #
+root = tk.Tk()
+app = Application(root)
+root.mainloop()
