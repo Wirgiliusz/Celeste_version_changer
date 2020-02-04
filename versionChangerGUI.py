a96d26e39277f6dae63fb94411e14f259bc84707
Wirgiliusz <kamx98@gmail.com>
2020-02-04 22:31:16 +0100

    Added icons

--- versionChangerGUI.py
+++ versionChangerGUI.py
@@ -2,27 +2,45 @@ import shutil
 import pathlib
 import tkinter as tk
 from tkinter import ttk
+from PIL import ImageTk, Image
 
 
 class Application:
     orginal_exe_path = pathlib.Path("./Celeste_Test/orgi/CelesteEXE")
     everest_exe_path = pathlib.Path("./Celeste_Test/oeve/CelesteEXE")
     destination_path = pathlib.Path("./Celeste_Test/CelesteEXE")
+    orginal_icon_path = pathlib.Path("./Icons/celeste_icon.jpg")
+    everest_icon_path = pathlib.Path("./Icons/everest_icon.jpg")
+    load_image_orginal = None
+    load_image_everest = None
+    icon = None
+    icon_Label = None
     actualVersion = "Unknown"
 
     def changeToOrginal(self):
         shutil.copy2(self.orginal_exe_path, self.destination_path)
+        self.actualVersion = "Orginal"
+        self.icon = ImageTk.PhotoImage(self.load_image_orginal)
+        self.icon_Label.config(image=self.icon)
         print("Game switched to Orginal version")
 
     def changeToEverest(self):
         shutil.copy2(self.everest_exe_path, self.destination_path)
+        self.actualVersion = "Everest"
+        self.icon = ImageTk.PhotoImage(self.load_image_everest)
+        self.icon_Label.config(image=self.icon)
         print("Game switched to Everest version")
 
     def __init__(self, root):
-        root.geometry("400x100")
+        root.geometry("360x90")
         root.title("Celeste version changer")
 
+        self.load_image_orginal = Image.open(self.orginal_icon_path)
+        self.load_image_everest = Image.open(self.everest_icon_path)
+
         ttk.Label(text="What game version do you want to play?").grid(row=0,column=0,columnspan=2,padx=10,pady=15)
+        self.icon_Label = ttk.Label(image=self.icon)
+        self.icon_Label.grid(row=0,column=2,rowspan=2,padx=5)
         ttk.Button(command=self.changeToOrginal, text="Orginal Celeste", width=15).grid(row=1,column=0,padx=5,pady=5)
         ttk.Button(command=self.changeToEverest, text="Everest Celeste", width=15).grid(row=1,column=1,padx=5,pady=5)
 
